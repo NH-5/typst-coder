620f966d0e51f27d0600013f185b11de2ded4e4c
NH5 <NH5wuzheng@outlook.com>
2026-04-21 16:33:52 +0800

    reshuffle files

---
+++ downloader/model_download.py
@@ -0,0 +1,9 @@
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).parent.parent
+
+snapshot_download(
+    repo_id='Qwen/Qwen3.5-0.8B-Base',
+    local_dir=PROJECT_PATH / 'model'
+)
