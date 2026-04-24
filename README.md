435ac2979ff869945be526dca74ab14d19d23644
NH5 <NH5wuzheng@outlook.com>
2026-04-24 17:00:46 +0800

    update README and download code

---
+++ README.md
@@ -0,0 +1,3 @@
+这个项目基于qwen3.5-0.8b-base, 想在此基础上训练得到一个typst-coder. 
+
+数据集来自HuggingFace.
---
+++ data_download.py
@@ -0,0 +1,17 @@
+from huggingface_hub import hf_hub_download
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).parent.parent
+
+hf_hub_download(
+    repo_id='TechxGenus/Typst-Train',
+    filename='typst_train.json',
+    repo_type='dataset',
+    local_dir= PROJECT_PATH / 'data/raw/train/'
+)
+hf_hub_download(
+    repo_id='TechxGenus/Typst-Test',
+    filename='typst_test.json',
+    repo_type='dataset',
+    local_dir=PROJECT_PATH / 'data/raw/test/'
+)
